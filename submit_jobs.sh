17859a51f1eac3af9c2d34b3d27328900ea343a0
pmocz <philip.mocz@gmail.com>
2025-08-01 16:07:51 -0400

    job time limits

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Submit jobs to rusty
-sbatch --time=0-00-01:00 sbatch_rusty.sh 32
-sbatch --time=0-00-01:00 sbatch_rusty.sh 64
-sbatch --time=0-00-01:00 sbatch_rusty.sh 128
-sbatch --time=0-00-01:00 sbatch_rusty.sh 256
-sbatch --time=0-00-08:00 sbatch_rusty.sh 512
+sbatch --time=0-00:10 sbatch_rusty.sh 32
+sbatch --time=0-00:20 sbatch_rusty.sh 64
+sbatch --time=0-01:00 sbatch_rusty.sh 128
+sbatch --time=0-01:00 sbatch_rusty.sh 256
+sbatch --time=0-01:00 sbatch_rusty.sh 512
