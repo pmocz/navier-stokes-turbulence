72f16ae9f7bcab10d7a1dce357772ed9308c8feb
pmocz <philip.mocz@gmail.com>
2025-10-15 13:22:02 -0400

    add sbatch skewers

---
+++ sbatch_skewers.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+#SBATCH --job-name=ns-skewers
+#SBATCH --output=slurm-%j.out
+#SBATCH --error=slurm-%j.err
+#SBATCH --partition gpu
+#SBATCH --constraint=h100
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-gpu=80G
+#SBATCH --time=00-01:00
+
+module purge
+module load python/3.11
+
+export PYTHONUNBUFFERED=TRUE
+
+source $VENVDIR/navier-stokes-turbulence-venv/bin/activate
+
+echo "resolution: $1"
+
+srun --cpu-bind=cores python analyze_skewers.py --res $1
